4da8e4fac4df47da229d5e9c9ccea2ba57a3f4e3
Mihir Samanyu Talamudipi <mtalamud@purdue.edu>
2026-01-09 20:45:17 +0530

    Add popup function for app information
    This program envisions advanced microprocessors with distinct personalities and capabilities, featuring Turtle-based visual designs and an interactive chatbot.

---
+++ ENGR133_Project_About_UDF.py
@@ -0,0 +1,8 @@
+import tkinter as tk
+from tkinter import messagebox
+def popup():
+    messagebox.showinfo("App Info", '''This program envisions the potential of advanced microprocessors with distinct personalities and capabilities.
+It features Turtle-based visual designs representing four unique microprocessors combined with an interactive chatbot to visualize life.
+
+While currently in its early stages, the program showcases a creative concept with significant potential for future expansion and sophistication.
+''')
